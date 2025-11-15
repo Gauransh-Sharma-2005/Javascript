1b08d43f72ac55ca82420c9ce435e87819735d1a
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2025-11-15 23:50:39 +0530

    basics of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// const --> cannot be changed
+// var --> generally not used
+// let --> 
+
+const accountId = 144553
+let accountEmail =  "xyz@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2  "Not Allowed"
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121245"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])  // Displays items in a tabular format
