116537b844f3c6fe3f96e4d5268c1e866ab424c0
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2025-11-16 00:10:38 +0530

    learning about datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+// code readability is priority
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 )  "will give error since we are using node.js and not not browser"
+
+console.log(3+3)
+
+let name = "hitesh"
+let age = 20
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53 (range)
+// bigint => for bigger numbers
+// string => ""
+// boolean => true or false
+// null =>  standalone value (representation of an empty value)
+// undefined => when the value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "ansh");
+console.log(typeof null);  // object
+console.log(typeof undefined); //undefined
+
